ffd4a9b5e4f8f3da4373c3379c79906656757bd2
gustavoemf <gustavoeduardomf@gmail.com>
2023-06-15 20:29:41 -0400

    Marca e Modelo client

--- src/client/marca-client.ts
+++ src/client/marca-client.ts
@@ -1,3 +1,4 @@
+import { Marca } from '@/model/marca';
 import axios, { AxiosInstance } from 'axios';
 
 export class MarcaClient {
@@ -8,5 +9,50 @@ export class MarcaClient {
             baseURL: 'http://localhost:8080/api/marca',
             headers: {'Conten-type' : 'application/json'}
         })
+    }
+    
+    public async findById(id: number) : Promise<Marca> {
+        try{
+            return(await this.axiosClient.get<Marca>(`?id=${id}`)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async findAll() : Promise<Marca[]> {
+        try {
+            return (await this.axiosClient.get<Marca[]>(`/lista`)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async cadastrar(marca: Marca) : Promise<void> {
+        try {
+            return (await this.axiosClient.post(``, marca)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+    
+    public async atualizar(marca: Marca) : Promise<void> {
+        try{
+            return (await this.axiosClient.put(`/${marca.id}`, marca)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async excluir(marca: Marca) : Promise<void> {
+        try{
+            return (await this.axiosClient.delete(`/${marca.id}`)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
     }
 }
--- src/client/modelo-client.ts
+++ src/client/modelo-client.ts
@@ -1,3 +1,4 @@
+import { Modelo } from "@/model/modelo";
 import axios, { AxiosInstance } from "axios";
 
 
@@ -9,5 +10,50 @@ export class ModeloClient {
             baseURL: 'http://localhost:8080/api/modelo',
             headers: {'Content-type' : 'application/json'}
         })
+    }
+
+    public async findById(id: number) : Promise<Modelo> {
+        try {
+            return (await this.axiosClient.get<Modelo>(`?id=${id}`)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async findAll() : Promise<Modelo[]> {
+        try {
+            return (await this.axiosClient.get<Modelo[]>(`/lista`)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async cadastrar(modelo: Modelo) : Promise<void> {
+        try {
+            return (await this.axiosClient.post(``, modelo)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+    
+    public async atualizar(modelo: Modelo) : Promise<void> {
+        try {
+            return (await this.axiosClient.put(`/${modelo.id}`, modelo)).data
+        }
+        catch(error:any){
+            return Promise.reject(error.response)
+        }
+    }
+
+    public async excluir(modelo: Modelo) : Promise<void> {
+        try {
+            return (await this.axiosClient.delete(`/${modelo.id}`)).data
+        }
+        catch(error:any) {
+            return Promise.reject(error.response)
+        }
     }
 }
